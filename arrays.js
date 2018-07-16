0b81edee5c18daf46cda282437837c9d5ab6599e
Edward Castillo <edwardcastillo@me.com>
2018-07-16 22:02:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,21 +1,21 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ];
 
 function addElementToBeginningOfArray(array, element) {
-  var newArray = [element, ...array];
+  var newArray = [element, ...array]; // adds to beginning of array without mutating/altering the original
   return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
+  array.unshift(element); // adds to beginning of array by mutating/altering the original
   return array;
 }
 
 function addElementToEndOfArray(array, element) {
-  var newArray = [...array, element];
+  var newArray = [...array, element]; // adds to end of array without mutating/altering the original
   return newArray;
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element);
+  array.push(element); // adds to end of array by mutating/altering the original
   return array;
 }
